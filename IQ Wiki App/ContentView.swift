24dac26eb65fe82d6b183c548470ad140e6cade5
zkPeep <epiqgames@proton.me>
2024-03-15 18:02:58 -0500

    Initial Commit

---
+++ IQ Wiki App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IQ Wiki App
+//
+//  Created by zkpeep on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IQ Wiki App/IQ_Wiki_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IQ_Wiki_AppApp.swift
+//  IQ Wiki App
+//
+//  Created by zkpeep on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct IQ_Wiki_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
